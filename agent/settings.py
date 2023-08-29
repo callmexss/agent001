20d76dd6056ca477d6e7ee240b676d4b878d58be
callmexss <callmexss@126.com>
2023-08-29 20:52:20 +0800

    chore: add db settings.

--- agent/settings.py
+++ agent/settings.py
@@ -4,6 +4,8 @@ _CUR = Path(__file__).parent
 PROJECT_ROOT = _CUR.parent
 CHAT_HISTORY_PATH = PROJECT_ROOT / "chat_history"
 DB_PATH = PROJECT_ROOT / ".vector_db"
+CHROMA_PATH = DB_PATH / "chroma"
+FAISS_PATH = DB_PATH / "faiss"
 PAPERS_PATH = PROJECT_ROOT / "data/papers"
 WECHAT_POST_PATH = PROJECT_ROOT / "data/articles"
 
